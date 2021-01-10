e7b35160c7725e63730c574b9fa0d7b152e83760
Natanel Young <46653502+natanelyoung@users.noreply.github.com>
2021-01-10 11:31:30 +0200

    my example

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <h1>All Events</h1>
+    <div class="event" v-for="event in events" :key="event._id">
+      <h2>{{event.title}}</h2>
+      <p>{{event.date}}</p>
+    </div>
+  <login :db="db"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Bagel from '@bageldb/bagel-db';
+import Login from './login';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  components:{Login},
+  data(){
+    return{
+      db:null,
+      events:[]
+    }
+  },
+  mounted(){
+    this.db = new Bagel(process.env.VUE_APP_BAGEL_TOKEN)
+    this.getEvents()
+  },
+  methods:{
+    async getEvents(){
+      let {data:events} = await this.db.collection("events").get()
+      this.events = events;
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+
+<style scoped>
+#app{
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
